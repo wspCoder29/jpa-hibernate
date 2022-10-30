803c43f9dd48f0b5307041b5d25090509b8634a5
unknown <leandro.wsp@gmail.com>
2022-10-30 15:57:24 -0300

    Inicio projeto JPA Hibernate

---
+++ ProjetoJPA-Hibernate/aulajpa/src/aplicacao/Programa.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(1, "Leandro", "leandro@gmail.com");
+		Pessoa p2 = new Pessoa(2, "Ada", "ada@gmail.com");
+		Pessoa p3 = new Pessoa(3, "Ifa", "ifa@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
